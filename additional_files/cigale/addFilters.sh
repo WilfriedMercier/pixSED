20d48ea36fae7be10cb2218694ab622c33e59603
Wilfried Mercier <wilfried.mercier58@gmail.com>
2022-02-24 15:55:31 +0100

    corrected an issue

--- additional_files/cigale/addFilters.sh
+++ additional_files/cigale/addFilters.sh
@@ -2,7 +2,7 @@
 # MERCIER Wilfried - IRAP
 # A bash wrapper to automatically add the filters in filter directory into Cigale database
 
-for filter in $(ls filters);
+for filter in $(find filters -type f);
 do
    pcigale-filters add "$filter"
 done
