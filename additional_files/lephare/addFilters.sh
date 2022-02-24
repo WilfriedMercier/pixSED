0c5e884fe412d08eeee625e5ea3c77c3a8faac60
Wilfried Mercier <wilfried.mercier58@gmail.com>
2022-02-24 16:23:55 +0100

    small script to add filters into LePhare

---
+++ additional_files/lephare/addFilters.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# MERCIER Wilfried - IRAP
+# A bash wrapper to automatically add the filters in filter directory into LePhare database
+
+cp -r hst_filters "$LEPHAREDIR"/filt
+exit 0
