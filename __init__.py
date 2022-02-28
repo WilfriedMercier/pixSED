d61450132e557f21cc22a5c2c943f7ff73bae328
Wilfried Mercier <wilfried.mercier58@gmail.com>
2022-02-28 16:25:36 +0100

    update

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 r"""
-.. codeauthor:: Hugo Plombat - LUPM <hugo.plombat@umontpellier.fr> & Wilfried Mercier - IRAP <wilfried.mercier@irap.omp.eu>
+.. codeauthor:: Hugo Plombat - LUPM <hugo.plombat@umontpellier.fr> & Wilfried Mercier - IRAP <wilfried.mercier@irap.omp.eu> & Maxime Tarrasse - IRAP <Maxime.Tarrasse@irap.omp.eu>
 
 Init file for the SED fitting parser library.
 """
@@ -12,3 +12,4 @@ from .catalogues import LePhareCat, CigaleCat
 from .outputs    import LePhareOutput, CigaleOutput
 from .misc       import SEDcode, CleanMethod, MagType, TableFormat, TableType, TableUnit, YESNO, ANDOR, IMF
 from .misc       import cigaleModules as cigmod
+from .photometry import countToMag, MagTocount, countToFlux
